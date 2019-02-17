947d33337c43fe09f084f718fd1f5e7346b6d870
SemanticBeeng <nick@semanticbeeng.com>
2019-02-17 12:45:35 -0800

    build update

--- src/main/scala/freestyle/Main.scala
+++ src/main/scala/freestyle/Main.scala
@@ -17,20 +17,20 @@
 package freestyle
 
 import cats.implicits._
-
-import freestyle._
-import freestyle.implicits._
+import freestyle.free._
+import freestyle.free.implicits._
+//
+import algebra._
+import algebra.implicits._
+//
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Await
 
-import algebra._
-import algebra.implicits._
-
 object Main extends App {
 
-  def program[F[_]: AlgebraM]: FS[F, String] =
+  def program[F[_]: AlgebraM]: FreeS[F, String] =
     AlgebraM[F].hello
 
   println(Await.result(program[AlgebraM.Op].interpret[Future], Duration.Inf))
